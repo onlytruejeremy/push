84e18c5c5e4f2125530f33b123ecc64e488416e0
Jeremy Barber <jeremydev2020@gmail.com>
2022-07-07 07:52:24 -0700

    Setup The Landing Page and Nav Bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,45 +1,39 @@
 import React from "react";
-import { FiVideo, FiMenu, FiX, FiArrowRight } from "react-icons/fi";
-import useToggle from "../../hooks/useToggle";
-import Link from "../Link/Link";
+import { NavLink } from "react-router-dom";
+import { FiVideo, FiMenu, FiX } from "react-icons/fi";
 import "./NavBar.css";
+import useToggle from "../../hooks/useToggle";
 const NavBar = () => {
   const { open, toggle } = useToggle();
   return (
-    <header className="nav-wrapper whitespace-nowrap">
-      <nav className="nav">
-        <Link
-          href="/"
-          className="inline-flex items-center gap-2 mr-8 text-xl"
-        >
-          <FiVideo className="text-2xl" />
+    <header className="header">
+      <nav className={`nav ${open && "nav--active"}`}>
+        <NavLink to="/" className="nav__logo">
+          <FiVideo className="self-center text-2xl" />
           <span>Push</span>
-        </Link>
-        <div className={`nav-menu ${open && "active"}`}>
-          <div className="flex flex-col w-full gap-4 md:flex-row">
-            <Link href="#" className="nav-link">
+        </NavLink>
+        <div className="nav__lists">
+          <div className="nav__items nav__items--left">
+            <NavLink to="/" className="nav__link">
               Overview
-            </Link>
-            <Link href="#" className="nav-link">
+            </NavLink>
+            <NavLink to="/" className="nav__link">
               FAQs
-            </Link>
+            </NavLink>
           </div>
-          <div className="flex flex-col w-full gap-4 md:items-center md:flex-row md:justify-end">
-            <Link href="/login" className="nav-link">
+          <div className="nav__items nav__items--right">
+            <NavLink to="/" className="nav__link">
               Sign In
-            </Link>
-            <Link href="/join" className="btn btn-primary">
-              <span>Join Meeting</span>
-            </Link>
-            <Link href="/register" className="btn btn-secondary">
+            </NavLink>
+            <NavLink to="/" className="btn btn--secondary">
+              Join Meeting
+            </NavLink>
+            <NavLink to="/" className="btn btn--primary">
               Get Started
-            </Link>
+            </NavLink>
           </div>
         </div>
-        <button
-          onClick={toggle}
-          className="ml-auto text-2xl hover:text-gray-200 md:hidden"
-        >
+        <button className="nav__toggle" onClick={toggle}>
           {open ? <FiX /> : <FiMenu />}
         </button>
       </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,40 @@
 import React from "react";
-import Link from "../components/Link/Link";
-
+import { FiKey } from "react-icons/fi";
+import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
-    <>
-      <section className="flex flex-col-reverse items-center justify-center h-[calc(100vh-50px)] gap-8 px-4 py-10 mx-auto text-gray-200 md:flex-row xl:px-0 max-w-7xl">
-        <div className="flex flex-col items-center justify-center text-left md:text-left md:items-start md:justify-start">
-          <h1 className="mt-0 mb-2 text-5xl font-medium leading-tight text-gray-200">
-            Video calling and meetings for anyone, anywhere.
-          </h1>
-          <p className="mt-0 mb-2 text-base font-medium leading-tight text-gray-300">
-            Push makes it easy to host or join virtual meetings and
-            it's 100% free. Try it yourself!
-          </p>
-          <div className="flex gap-2 mt-8">
-            <button className="btn btn-secondary">Get Started</button>
-            <div className="inline-flex">
-              <input type="text" placeholder="Enter Meeting Code" />
-              <button className="btn btn-primary">Join</button>
-            </div>
-          </div>
-          <p className="self-start mt-8 text-gray-300">
-            Don't have an account?
-            <Link href="/" className="ml-2 underline">
-              Sign Up
-            </Link>
-          </p>
+    <section className="gap-8 min-h-[calc(100vh-48px)] grid grid-cols-1 p-6 mx-auto text-gray-200 max-w-7xl  items-center content-center md:grid-cols-2">
+      <div className="order-2 md:order-1">
+        <h1 className="mt-0 mb-2 text-5xl font-medium leading-tight text-gray-200">
+          Video calling for everyone
+        </h1>
+        <p className="mt-4">
+          Push gives you the tools you need to connect with friends,
+          family, and co-workers no matter where you are in the world.
+        </p>
+        <div className="flex flex-row gap-2 py-8 hero-actions">
+          <button className="btn btn--primary">Get Started</button>
+          <label className="label">
+            <span className="inline-block px-2 my-auto text-xl text-gray-600 bg-transparent">
+              <FiKey className="my-auto" />
+            </span>
+            <input type="text" placeholder="Enter Meeting Code" />
+          </label>
         </div>
-        <div className="min-h-[300px] max-h-[40vh]">
-          <img
-            className="object-cover w-full h-full rounded-md shadow-lg shadow-blue-500/2"
-            src="https://images.unsplash.com/photo-1586543354240-2187898bb2e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          />
-        </div>
-      </section>
-    </>
+        <p className="text-gray-400">
+          Need an account?{" "}
+          <NavLink to="/register" className="hover:text-gray-200">
+            Sign Up
+          </NavLink>
+        </p>
+      </div>
+      <div className="order-1 md:order-2">
+        <img
+          className="object-cover max-h-[500px] w-full shadow-blue-900/50 shadow-xl rounded-md"
+          src="https://images.unsplash.com/photo-1612831817984-97e394106fff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+        />
+      </div>
+    </section>
   );
 };
 
